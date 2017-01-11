6869da4d15a64a9f10a9815efaeaa56db702813e
SBKubric <sbkubric@gmail.com>
2017-01-11 16:30:25 +0300

    Added owl check

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,3 +1,19 @@
+from pytz import timezone
+from datetime import datetime, timedelta
+
+
+def get_local_datetime(timestamp, str_time_zone):
+    my_time_zone = timezone(str_time_zone)
+    loc_dt = my_time_zone.localize(datetime.fromtimestamp(timestamp))
+    return loc_dt
+
+
+def is_owl_time(local_datetime):
+    if 0 <= local_datetime.hour < 6:
+        return True
+    return False
+
+
 def load_attempts():
     pages = 1
     for page in range(pages):
